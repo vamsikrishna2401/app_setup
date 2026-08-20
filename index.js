18a26dd8fc1838498cdbf64284b242c15387c991
vamsikrishna2401 <vamsikrishna2401@gmail.com>
2026-08-20 12:43:31 +0530

    adding index.js file

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello from GitHub Actions!");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from GitHub Actions!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
